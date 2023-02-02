812eebd142951c1558edf5becf4fe5693ce57244
want8607 <gotjd8607@gmail.com>
2023-02-02 21:27:35 +0900

    [app] AirLevel클래스 생성

---
+++ app/src/main/java/com/phil/airinkorea/model/AirLevel.kt
@@ -0,0 +1,12 @@
+package com.phil.airinkorea.model
+
+enum class AirLevel(val value: String) {
+    Level1("Excellent"),
+    Level2("Good"),
+    Level3("Fine"),
+    Level4("Moderate"),
+    Level5("Poor"),
+    Level6("Bad"),
+    Level7("Unhealthy"),
+    Level8("Terrible")
+}

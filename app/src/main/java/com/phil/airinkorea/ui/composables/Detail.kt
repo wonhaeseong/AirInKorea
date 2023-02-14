d5b7ec0b933ceac6cef147df22d89f64ecf2b1bc
want8607 <gotjd8607@gmail.com>
2023-02-14 23:19:34 +0900

    [app] Detail 수정

--- app/src/main/java/com/phil/airinkorea/ui/composables/Detail.kt
+++ app/src/main/java/com/phil/airinkorea/ui/composables/Detail.kt
@@ -1,4 +1,4 @@
-package com.phil.airinkorea.ui.component
+package com.phil.airinkorea.ui.composables
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
@@ -7,15 +7,16 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.phil.airinkorea.R
-import com.phil.airinkorea.ui.icon.AikIcons
+import com.phil.airinkorea.model.DetailData
+import com.phil.airinkorea.ui.commoncomponents.ExpendableTitleBar
 import com.phil.airinkorea.ui.theme.*
 
 enum class DetailType {
@@ -23,9 +24,9 @@ enum class DetailType {
     Sub
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFF001FC5)
 @Composable
-fun AikDetails(
+fun Detail(
+    detailData: DetailData,
     modifier: Modifier = Modifier
 ) {
     var expandedState by remember {
@@ -34,74 +35,34 @@ fun AikDetails(
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(10.dp)
     ) {
-        DetailsBar(
-            expandedState = expandedState
-        ) { expandedState = !expandedState }
-        Spacer(modifier = Modifier.size(8.dp))
-        DetailExpandableCard(
-            expandedState = expandedState
-        ) { expandedState = !expandedState }
-    }
-}
-
-@Composable
-fun DetailsBar(
-    expandedState: Boolean,
-    modifier: Modifier = Modifier,
-    onClick: () -> Unit
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = modifier
-            .fillMaxWidth()
-    ) {
-        Text(
-            text = "Details",
-            color = Color.White,
-            style = MaterialTheme.typography.subtitle1
-        )
-        if (expandedState) {
-            IconButton(
-                onClick = onClick,
-                modifier = Modifier
-                    .size(24.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = AikIcons.ArrowDropUp),
-                    contentDescription = null,
-                    tint = Color.White
-                )
-            }
-        } else {
-            IconButton(
-                onClick = onClick,
-                modifier = Modifier
-                    .size(24.dp)
-            ) {
-                Icon(
-                    imageVector = AikIcons.ArrowDropDown,
-                    contentDescription = null,
-                    tint = Color.White
-                )
-            }
-        }
+        ExpendableTitleBar(
+            expandedState = expandedState,
+            titleText = "Details",
+            onClick = { expandedState = !expandedState })
+        Spacer(modifier = Modifier.size(5.dp))
+        ExpendableDetailsGrid(
+            detailData = detailData,
+            expandedState = expandedState,
+            onClick = { expandedState = !expandedState })
     }
 }
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun DetailExpandableCard(
+fun ExpendableDetailsGrid(
+    detailData: DetailData,
     expandedState: Boolean,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    Card(
+    Surface(
         onClick = onClick,
-        shape = Shapes.medium,
-        modifier = Modifier
+        elevation = 2.dp,
+        shape = MaterialTheme.shapes.medium,
+        modifier = modifier
             .fillMaxWidth()
+            .wrapContentHeight()
     ) {
         Column(
             modifier = Modifier
@@ -114,11 +75,14 @@ fun DetailExpandableCard(
                     .height(IntrinsicSize.Min)
                     .fillMaxWidth()
             ) {
-                DetailsForm(
+                DetailLayout(
                     title = stringResource(id = R.string.pm_10),
-                    value = "Very\nGood",
-                    unit = stringResource(id = R.string.micro_per_square_meter, 5),
-                    sourceColor = Color.Blue,
+                    level = detailData.pm10Level,
+                    value = stringResource(
+                        id = R.string.micro_per_square_meter,
+                        detailData.pm10Value
+                    ),
+                    sourceColor = detailData.pm10Color,
                     type = DetailType.Main,
                     modifier = Modifier
                         .weight(1f)
@@ -130,11 +94,14 @@ fun DetailExpandableCard(
                         .fillMaxHeight()
                         .width(1.dp)
                 )
-                DetailsForm(
+                DetailLayout(
                     title = stringResource(id = R.string.pm_2_5),
-                    value = "Bad",
-                    unit = stringResource(id = R.string.micro_per_square_meter, 5),
-                    sourceColor = Color.Blue,
+                    level = detailData.pm25Level,
+                    value = stringResource(
+                        id = R.string.micro_per_square_meter,
+                        detailData.pm25Value
+                    ),
+                    sourceColor = detailData.pm25Color,
                     type = DetailType.Main,
                     modifier = Modifier
                         .weight(1f)
@@ -154,11 +121,11 @@ fun DetailExpandableCard(
                         .fillMaxWidth()
                         .height(IntrinsicSize.Min)
                 ) {
-                    DetailsForm(
+                    DetailLayout(
                         title = stringResource(id = R.string.no2),
-                        value = "Very\nGood",
-                        unit = stringResource(id = R.string.ppm, 5),
-                        sourceColor = Color.Blue,
+                        level = detailData.no2Level,
+                        value = stringResource(id = R.string.ppm, detailData.no2Value),
+                        sourceColor = detailData.no2Color,
                         type = DetailType.Sub,
                         modifier = Modifier
                             .weight(1f)
@@ -169,11 +136,11 @@ fun DetailExpandableCard(
                             .fillMaxHeight()
                             .width(1.dp)
                     )
-                    DetailsForm(
+                    DetailLayout(
                         title = stringResource(id = R.string.so2),
-                        value = "UnHealthy",
-                        unit = stringResource(id = R.string.ppm, 5),
-                        sourceColor = Color.Blue,
+                        level = detailData.so2Level,
+                        value = stringResource(id = R.string.ppm, detailData.so2Value),
+                        sourceColor = detailData.so2Color,
                         type = DetailType.Sub,
                         modifier = Modifier
                             .weight(1f)
@@ -184,11 +151,11 @@ fun DetailExpandableCard(
                             .fillMaxHeight()
                             .width(1.dp)
                     )
-                    DetailsForm(
+                    DetailLayout(
                         title = stringResource(id = R.string.CO),
-                        value = "Very\nGood",
-                        unit = stringResource(id = R.string.ppm, 5),
-                        sourceColor = Color.Blue,
+                        level = detailData.coLevel,
+                        value = stringResource(id = R.string.ppm, detailData.coValue),
+                        sourceColor = detailData.coColor,
                         type = DetailType.Sub,
                         modifier = Modifier
                             .weight(1f)
@@ -199,11 +166,11 @@ fun DetailExpandableCard(
                             .fillMaxHeight()
                             .width(1.dp)
                     )
-                    DetailsForm(
+                    DetailLayout(
                         title = stringResource(id = R.string.o3),
-                        value = "Bad",
-                        unit = stringResource(id = R.string.ppm, 5),
-                        sourceColor = Color.Blue,
+                        level = detailData.o3Level,
+                        value = stringResource(id = R.string.ppm, detailData.o3Value),
+                        sourceColor = detailData.o3Color,
                         type = DetailType.Sub,
                         modifier = Modifier
                             .weight(1f)
@@ -215,10 +182,10 @@ fun DetailExpandableCard(
 }
 
 @Composable
-fun DetailsForm(
+fun DetailLayout(
     title: String,
+    level: String,
     value: String,
-    unit: String,
     sourceColor: Color,
     type: DetailType,
     modifier: Modifier = Modifier
@@ -229,35 +196,39 @@ fun DetailsForm(
             verticalArrangement = Arrangement.SpaceBetween,
             modifier = modifier
                 .wrapContentHeight()
-                .padding(10.dp)
+                .padding(horizontal = 10.dp, vertical = 20.dp)
         ) {
             Text(
                 text = title,
-                style = MaterialTheme.typography.subtitle1
+                style = MaterialTheme.typography.subtitle1,
+                color = AIKTheme.colors.on_core_container
             )
-            Spacer(modifier = Modifier.size(10.dp))
+            Spacer(modifier = Modifier.size(5.dp))
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .height(80.dp)
             ) {
                 Text(
-                    text = value,
-                    style = MaterialTheme.typography.h5Normal,
+                    text = level,
+                    style = MaterialTheme.typography.h5,
+                    color = AIKTheme.colors.on_core_container,
                     textAlign = TextAlign.Center
                 )
             }
-            Spacer(modifier = Modifier.size(10.dp))
+            Spacer(modifier = Modifier.size(5.dp))
             Box(
                 modifier = Modifier
-                    .width(60.dp)
-                    .height(3.dp)
+                    .clip(MaterialTheme.shapes.medium)
+                    .width(70.dp)
+                    .height(6.dp)
                     .background(color = sourceColor)
             )
             Spacer(modifier = Modifier.size(7.dp))
             Text(
-                text = unit,
-                style = MaterialTheme.typography.body1
+                text = value,
+                style = MaterialTheme.typography.subtitle1,
+                color = AIKTheme.colors.on_core_container
             )
         }
     } else if (type == DetailType.Sub) {
@@ -266,36 +237,40 @@ fun DetailsForm(
             verticalArrangement = Arrangement.SpaceEvenly,
             modifier = modifier
                 .wrapContentHeight()
-                .padding(8.dp)
+                .padding(horizontal = 8.dp, vertical = 13.dp)
         ) {
             Text(
                 text = title,
                 style = MaterialTheme.typography.subtitle2,
-                fontWeight = FontWeight.Bold
+                color = AIKTheme.colors.on_core_container
             )
-            Spacer(modifier = Modifier.size(8.dp))
+            Spacer(modifier = Modifier.size(5.dp))
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .height(50.dp)
             ) {
                 Text(
-                    text = value,
+                    text = level,
                     style = MaterialTheme.typography.body1,
-                    textAlign = TextAlign.Center
+                    fontWeight = FontWeight.SemiBold,
+                    textAlign = TextAlign.Center,
+                    color = AIKTheme.colors.on_core_container
                 )
             }
             Spacer(modifier = Modifier.size(8.dp))
             Box(
                 modifier = Modifier
-                    .width(35.dp)
-                    .height(3.dp)
+                    .clip(MaterialTheme.shapes.medium)
+                    .width(45.dp)
+                    .height(4.dp)
                     .background(color = sourceColor)
             )
             Spacer(modifier = Modifier.size(6.dp))
             Text(
-                text = unit,
-                style = MaterialTheme.typography.body3
+                text = value,
+                style = MaterialTheme.typography.subtitle2,
+                color = AIKTheme.colors.on_core_container
             )
         }
     }
@@ -303,22 +278,29 @@ fun DetailsForm(
 
 @Preview
 @Composable
-fun DetailsBarPreview() {
-    var expandedState by remember {
-        mutableStateOf(false)
-    }
-    DetailsBar(
-        expandedState = expandedState
-    ) { expandedState = !expandedState }
-}
-
-@Preview(backgroundColor = 0xFF000A63)
-@Composable
-fun DetailsExpandableCardPreview() {
-    var expandedState by remember {
-        mutableStateOf(true)
-    }
-    DetailExpandableCard(expandedState = expandedState) {
-        expandedState = !expandedState
+fun DetailInfoPreview() {
+    AIKTheme(PollutionLevel.GOOD) {
+        Detail(
+            detailData = DetailData(
+                pm25Level = "Dangerous",
+                pm25Value = 10,
+                pm25Color = level1_core,
+                pm10Level = "Dangerous",
+                pm10Value =10,
+                pm10Color = level2_core,
+                no2Level = "Dangerous",
+                no2Value =10,
+                no2Color = level3_core,
+                so2Level = "Dangerous",
+                so2Value =10,
+                so2Color = level4_core,
+                coLevel = "Dangerous",
+                coValue =10,
+                coColor = level5_core,
+                o3Level = "Dangerous",
+                o3Value =10,
+                o3Color = level6_core
+            )
+        )
     }
 }

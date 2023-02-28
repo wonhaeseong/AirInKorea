7380ece89beec923f0f70443fd3a315c0f6e49b8
want8607 <gotjd8607@gmail.com>
2023-03-01 00:27:58 +0900

    [app] 앱 아이콘 변경

--- app/src/main/java/com/phil/airinkorea/ui/icon/AIkIcons.kt
+++ app/src/main/java/com/phil/airinkorea/ui/icon/AIkIcons.kt
@@ -1,8 +1,11 @@
 package com.phil.airinkorea.ui.icon
 
+import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.ui.res.painterResource
 import com.phil.airinkorea.R
 
 // ImageVector 출처 https://fonts.google.com/icons?icon.platform=android&icon.set=Material+Symbols
@@ -14,4 +17,7 @@ object AIKIcons {
     val ArrowDropUp = R.drawable.ic_arrow_drop_up
     val Error = R.drawable.ic_error_outline
     val GPSError = R.drawable.ic_gps_error
+    val Setting = Icons.Filled.Settings
+    val Info = R.drawable.ic_info
+    val AppIcon = R.drawable.ic_aik_foreground
 }

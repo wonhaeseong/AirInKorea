0d7bdbf43ede6efa7311be529e82cc819130d45b
want8607 <gotjd8607@gmail.com>
2023-05-11 22:34:46 +0900

    systembar 설정 변경

--- app/src/main/java/com/phil/airinkorea/ui/NavGraph.kt
+++ app/src/main/java/com/phil/airinkorea/ui/NavGraph.kt
@@ -1,9 +1,12 @@
 package com.phil.airinkorea.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.phil.airinkorea.ui.addlocation.AddLocationRoute
 import com.phil.airinkorea.ui.home.HomeRoute
 import com.phil.airinkorea.ui.managelocations.ManageLocationRoute
@@ -12,8 +15,15 @@ import com.phil.airinkorea.ui.managelocations.ManageLocationRoute
 fun NavGraph(
     navController: NavHostController
 ) {
+    val systemUiController = rememberSystemUiController()
     NavHost(navController = navController, startDestination = "Home") {
         composable("Home") {
+            SideEffect {
+                systemUiController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = false
+                )
+            }
             HomeRoute(
                 onManageLocationClick = { navController.navigate("ManageLocation") },
                 onParticulateMatterInfoClick = { navController.navigate("ParticulateMatterInfo") },
@@ -21,6 +31,12 @@ fun NavGraph(
             )
         }
         composable("ManageLocation") {
+            SideEffect {
+                systemUiController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = true
+                )
+            }
             ManageLocationRoute(
                 onBackButtonClick = { navController.popBackStack() },
                 onAddLocationButtonClick = {navController.navigate("AddLocation")}
